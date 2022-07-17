001db520f9c1f104d8892232870e25dc6d0b8a09
jumhuur <jumhuur123@hotmail.com>
2022-07-17 22:54:43 +0300

    17/07/2022

--- src/components/Profile/Order_s_details.js
+++ src/components/Profile/Order_s_details.js
@@ -376,7 +376,7 @@ function Gudoon(){
                         {oneOrder ? oneOrder.Mudada < 1 ? <li> <FontAwesomeIcon icon={faCircleCheck} className="i" /> Mudada : <span className="mudada">12</span> Saacadood</li> : <li> <FontAwesomeIcon icon={faCircleCheck} className="i" /> Mudada : <span className="mudada">{oneOrder.Mudada}</span> Maalin</li> : ""}
                         <li> <FontAwesomeIcon icon={faCircleCheck} className="i" /> Xadiga : <span className="xadiga_adeeg">{oneOrder && oneOrder.Xadiga}</span> {oneOrder && oneOrder.Nooca}</li>
                         <li> <FontAwesomeIcon icon={faCircleCheck} className="i" /> {oneOrder?  oneOrder.Qodobka1aad : ""} </li>
-                        <li> <FontAwesomeIcon icon={faCircleCheck} className="i" /> {oneOrder? oneOrder.Qodobka1aad : ""} </li>
+                        <li> <FontAwesomeIcon icon={faCircleCheck} className="i" /> {oneOrder? oneOrder.Qodobka2aad : ""} </li>
                     </ul>
                 </div>
                 <div className="iibiye_info">
