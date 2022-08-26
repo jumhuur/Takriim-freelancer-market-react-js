245529d75d26e43762ba330bc9f060f791e40c68
jumhuur <jumhuur123@hotmail.com>
2022-08-26 18:07:48 +0300

    26/08/2022

--- src/components/loading.js
+++ src/components/loading.js
@@ -2,9 +2,13 @@ function Loading({loading}){
     return(
         <>
         <div className={loading ? "over_log Load active":"over_log"} >
-        <div className="spinner">
-            <div className="double-bounce1"></div>
-            <div className="double-bounce2"></div>
+        <div class="sk-chase">
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
         </div>
         </div>
         </>
