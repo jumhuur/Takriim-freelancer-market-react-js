7eefc415b8fe7e49a417ec4f68c113c6ed86c47c
jumhuur <jumhuur123@hotmail.com>
2022-09-28 09:20:29 +0300

    28/09/2022

--- src/components/XeerarkaCusto.js
+++ src/components/XeerarkaCusto.js
@@ -12,115 +12,53 @@ function XeerCustomers(){
             {/* <!---------------biloga shaqooyinka -------------------> */}
             <div className="tranding_haye main">
                 <div className="Xerar">
-                    <h2>Xeerarka Iibsadaha (Customer)</h2>
+                    <h2>XEERARKA LOO SHAQEEYAHA(CUSTOMER - MACMIILKA).</h2>
                     <h3>Hordhac</h3>
                     <p>
-                    Ujeedada xeerarkan waa Hawl fudayndinta habsami u socodka shaqada shabakada takriim iyo ilaalinta xuquuqda shaqeeyaha iyo loo shaqeeyahaba iyo garashada qof walba oo ka hawl gala shabakada ama hawl ku fushada waxii xaq loogu leeyahay iyo waxa uu xaq u leeyahay isagu , 
-                    xeerarkani waxay u qaybsamayaan laba qaybood oo kala aha xeerarka loo shaqeeyahaya (macmiilka – customer) 
-                    Iyo xeerarka shaqaalaha xorta ah (freelancer)
+                    Ujeedada Xeerarkan Waa Hawl Fudaydinta Iyo Habsami U Socodka Shaqada Suuqa Takriim, Ilaalinta Xuquuqda Shaqaalaha, Loo Shaqeeyaha Iyo Garashada Qof Walba Oo Ka Hawl Gala Suuqa. Sidoo Kale In Aynu Kala Cadayno Qofkasta Wixii Xaq Loogu Leeyahay Iyo Waxa Uu Xaq U Leeyahay Isagu.
                     </p>
-                    <h3>Shuruuda Iyo Xerarka (Freelancer-ka)</h3>
+                    <h3>XEERARKA LOO SHAQEEYAHA(CUSTOMER - MACMIILKA).</h3>
                     <p className="qodob">
-                    1.	Shaqada uu soo galinayo Qofka (freelancer)-ka ah   shabakada ee uu ku dhex qabanayaa waa inay tahay mid aan wadan wax kasta oo shareecada islaamka ama dhaqanka wanaagsan ka hor imanaya sida waxyaabaha halkan ku xusan waa inay shaqadu ka madhantahay  .
-                    <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" /> Miisiga dhamaan noocyadiisa kala duwan lama ogola shabakada sifa kasta oo lagu shaqaysto ama uu ugu dhex muuqdo shabakadana lama ogola .
-                    <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" /> Sawirada dumarka aan asturnayn lama ogola waxaana laga wadaa aan asturnayn aan xidhnayn xijaabka ilaahay u sharciyeey ee watay dhamaan shuruudihii  8 ahaa  ee la yaqaanay daliilada kitaabka iyo Xadiiska rasuulkeenu   tusinayay .
-                    <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" /> Waxaa mamnuuca shaqo kasta oo Watada wax mukhadiraad ah sida qamri jaad ama sigaar noocyadiisa kala duwan  iyo dhamaan waxii kale ee sharcigeena islaamku aanu ogolayn .
+                    1.	Waa Inaad Si Wanaagsan Ula Shaqaysaaa Qofka Kuu Shaqaynaya (Freelancer).
                     <br/>
                     </p>
                     <p className="qodob">
-                    2.	Waa in uu freelacerku ula dhaqmo macmiilka si dadnimo iyo tix galin leh . 
+                    2.	Iska Hubso Macluumaadka Dalabka Sida.
+                    </p>
+                    <FontAwesomeIcon icon={faCircleCheck} className="i" />  Mudada
+                    <br/>
+                    <FontAwesomeIcon icon={faCircleCheck} className="i" /> 	Qiimaha 
+                    <br/>
+                    <FontAwesomeIcon icon={faCircleCheck} className="i" />  Xadiga
+                    <br/>
+                    <p className="qodob">
+                    3.	Macamiilku Isagaa Masuul Ka Ah Xadiga Dalabkiisa Iyo Mudada Uu Noqonayo Inta Uu Doonana, Wuu Ka Dhigan Karaaaa Waana Arin Uu U Xor Yahay, Macmiilka Iibsanaya Adeega Ka Mida Adeegyada Shabakada Takriim .
                     </p>
                     <p className="qodob">
-                    3.	Shaqada uu galinayo waa inay tahay mid uu  leeyahay qofka galinayaa  dhamaan macluumaadkeeduna isga u gaar yihiin lama ogola inuu freelancer ku  cid kale ka soo copy-garayso macluumaad sida ciwaanka , faahfahinta , sawirka shaqada iyo muuqaalka shaqadaada  laakiin waxii isku mid laga noqon karro waa la ogol yahay sida qiimaha iyo mudada  .
+                    4.	Hadii Dalabka Macmiilka Uu Aqbali Waayo Qofka Shaqadan La Dalbaday Leh Waxa Uu Macmiilka Iibsaday Xaq U Yeelayaa Inuu Ka Noqdo Dalabkaa Hadii Ay Ka Soo Wareegto Muddo <span>(3-Saacadood)</span> Ahi Lacagatii Dalabkaa Kaga Baxday Oo Aan Waxba Ka Dhinayana Waa Loo Celinayaa Macmiilka .
                     </p>
                     <p className="qodob">
-                    4.	Shaqooyinka uu freelancer ku  shabakada soo galin karaa ugu badnaan waa Afar shaqo  , Afar shaqo waxii ka badan lama ogola
+                    5.	Hadii Uu Qofka Shaqada Lihi Ka Laabto Dalabka, Macmiilku Waxa Uu Hellayaa Lacagtiisii Oo Dhamays Tiran. 
                     </p>
                     <p className="qodob">
-                    5.	Waa inuu freelancer ku ka fogaado waxa kasta oo suuqa ku keeni kara  sumcad xummo . 
+                    6.	Hadii Macmiilku Uu Dalbado Adeeg Kadib Uu Ka Laabto Sababta Uu Uga Laabtayna Aanay Ahayn  Sabab Rasmiya, Sida In Uu Wakhtigii Dhaafay Ama In La Aqbali Wayay, Lana Gaadhay Wakhtigii Loo Ogolaa Inuu Ka Laabato Ee Lagu Sheegay Qodobka Afraad,  Waxaa Lacagta Uu Ku Dalbay Ka Goaysa  <span>(1% )</span>Qiimaha Dalabkiisa .
                     </p>
                     <p className="qodob">
-                    6.	Hadii qiimayntaa la qiimeeyay freelancer ku ay hoos ugu dhacdo (2.50) waxaa la joojinayaa in muddo ah akoonka waxaana la diraasaynayaa sababta hadii maamulka shabakadu ku qanco in sidaa lagaaga joojiyo waa la joojinayaa. 
-                    hadii ay u arkaan in dib laguugu soo fasaxana waa laaguu soo fasaxi
-                    Freelancer ka  (2.50) qiimayntiisa hoos u dhaaftay macnaheedu waa qof badanka dadkii uu ushaqeeyay aanay ku qancin adeegiisa taasina waxay u soo jiidi kartaa shabakada sumcad xumo waana sababta loo hakinayo akoonkaaga .
+                    7.	Xaq Waxa Uu Macmiilku U Leeyahay Inta Aanu Gudoomin Wixii Loo Qabtay Ee Adeeg Ah,  Inuu Iska Hubiyo, Kadib Marka Uu U Arko Inay Yihiin Wixii Uu Rabay Iyo Sidii Uu Rabay, Inuu Gudoomo Isla Marka Uu Gudoomayana Waxay Kadhigantahay Waxa Uu Lacagtii Dalbkan Uu Ku Bixiyay U Fasaxay Qofkii U Shaqeeyay, Ku Noqosho Danbana Malaha. Taasina Waxay Muhiim Ka Dhigaysaa In Macmiilku Inta Aanu Gudoomin Dalabkiisa Uu Hubiya Hawsha Loo Qabtay . <br/>
                     </p>
                     <p className="qodob">
-                    7.	Waa in uu freelancer-ku uu dhamaystiro xogta akoonkiisa sida  <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	Sawir kur ah  <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	Magaca oo afran  <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	Kaadhka dhalashada marka loo baahdo  <br/>
-
+                    8. Heshiis Waa Dhex Mari Karaa Labada Qof Ee   Kala Iibsanaya Adeega Heshiiskaas oo Xaga  Mudada Ah,  Iyagoo Isticmaalaya Barta Wada Xidhiidhka Ee Shabakadu Ugu Tallo Gashay.  Heshiiskaasna Waxa Uu Khuseeyaa Kaliya Xaga Wakhtiga  Adeega Sida Inuu Qofka Dalabka Sameeyay Ee Macmiilka Ahi Uu Wakhti Ka Badan Kii Dalabka Uu Ku Sameeyay Uu Ugu Darro Qofka Adeegan U Qabanaya . <br/> 
                     </p>
                     <p className="qodob">
-                    8.	Qaabka lacagata uu ula bixi karro qofka freelancer ka ahi  waa <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" /> Zaad (telesom) <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" /> E-dahab (somtel) <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" /> Evc-Plus (Hormuud) <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" /> Gollis (Gollis) <br/>
- 
+                    9.	 Heshiis Kasta Oo Ay Wada Galaan Iibiyaha Adeega Iyo Iibsadaha Adeeg Fulintiisa Waxa Masuul Ka Ah Labad Qof Ee Heshiiskaas Gallay Hadii Mid Kamida Heshiiskaa Ka Baxo Maamulka Suuqu Masuul Kama Aha. Kaliya Maamulku Waxa uu  Tix Galinayaa Wakhtiga Ku Qoran Aqoonsiga Dalabka .
                     </p>
                     <p className="qodob">
-                    9.	Looma ogola qof kaliya wax kabadan hal akoon 
-                    </p>
-                    <p className="qodob">
-                    10.	Akoonk freelancer ku  waa qasnada lacagta Qofka freelancer ka ahi uu ka hellay  shaqada uu ku qabtay shabakada dhexdeeda . 
- ilaalinta Lacagtaasi isga ayaa masuul ka ah . 
+                    10.	Waxaa Mamuuc Ah Ina Labada Qof Ee Wax Ku Kala Iibsanaya Shabakada Ay Isticmaalaan Waxyaabo Lagu Wada Xidhiidho Oo Ka Baxsan Shabkada Sida Inay Lanbarada Kala Qortaan, Ama Akoonada Shabakada Ay Ku Wada Xidhiidhaan Dad Waynuhu Kadibna Iyagu Si Gaara U Wada Hadlaan Arinkaasi Waa Mid Mamnuuca Kana Hor Imanaya Halku Dhigyada Suuqa . 
 
                     </p>
                     <p className="qodob">
                     11.	Macluumaadkaga (data) cid aan la wadaagaynaa ma jirta cid aan u gubinaynaana ma jirto hadii loo baahdo in cid loo gudbiyana adaa lagaa ogolaasho qaadanayaa marka laga reebo waxii arrimo amin ah ee looga baahdo macluumaadkaaga .
                     </p>
-                    <p className="qodob">
-                    12.	Hadii aad u baahato inaad akoonka hawada ka saarto waxaa la soo xidhiidhi kartaa shabakada adoo soo qoraya codsigaaga shaqo joojineed iyo akoon Joojineedba kadibna muddo ku siman hal todobaad maamulka shabakadu waxa uu kuu fulinayaan codsiga shaqo joojineed iyo akoon joojineedba waa hadii aad wax xuquuqa ah la xidhiidhin akoonkaa sida in dalabyo aad dhamayn jiraan oo sugaya inaad dhamayso  iwm .
-                    </p>
-                    <p className="qodob">
-                    13.	qiimo u samaynta iyo sidoo kale muddada adeega  waxaa  u Xorra Qofka shaqada soo gallinaya  cidna  kuguma qasabayso qiimo gaara iyo wakhti gaara Waayo suuqan waa suuqa shaqaalaha Xorta ah . 
-                    </p>
-                    <p className="qodob">
-                    14.	lacagta Uu ka hellay  shaqada uu Qabtay Freelancer ku  waxay si toosa kuugu dhacaysaa akoonka aad ku leedahaya shabakada . 
-                    </p>
-                    <p className="qodob">
-                    15.	marka ugu horaysa ee uu  lacagta hello freelancerku  ma awoodayo inuu lacagta la wareego  ama uud furato  laakiin way xidhanaanaysaa lacagtaas ilaa lagaadhayo wakhtiga lacag furashada . 
-                    </p>
-                    <p className="qodob">
-                    16.	wakhtiga lacag furashadu waa sadex jeer bishiiba oo kala ah <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	10 -Bisha  . <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	20-bisha . <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	30-bisha  . <br/>
-                    </p>
-                    <p className="qodob">
-                    17.	lacagta u furan waxa uu  xor u yahay Qofka freelancer ka ahi   inuu la baxdo wakhtiga uu doono  Marka Muddo <span>(10)</span> saac gudaheedna waxa uu helli doonaa lacagtaas uu dalbaday.
-                    </p>
-                    <p className="qodob">
-                    18.	shaqo kasta oo uu  ku dhex  qabto shabakada qofka freelancer ka ahi waxay Shabakadu ku leedahay  <span>15%</span> qadark kasta oo ay tahay
-                    <span> 85% </span> soo hadhayna waxaa iska leh freelacer-ka .
-                    </p>
-                    <p className="qodob">
-                    19.	dalab kasta oo u yimaada freelancer-ka  waxaa laaga doonayaa inuu  ku aqballo muddo ka yar <span>(3)</span>-saac hadii <span>(3)</span>-saac dalabkaagaasi aqbalaaad la,aan  noqodo qofka soo dalbaday waxa uu xaq u yeelanayaa inuu ka laabto dalabkaa haduu ka laabtana qofka shaqada la soo dalbaday lahaa  ayaa rasiidkiisa laga jarayaa <span>1.5% </span> qiimaha dalabkaasi laga laabtay uu ahaaa .
-                    </p>
-                    <p className="qodob">
-                    20.	Hadii dalabka uu hawshiisa waday qofka (freelancerka) ahi uu wakhtigiisu dhamaado walina aanu dhamaan xaq waxa uu u leeyahay qofkii ka soo dalbaday inuu ka laabto dalabkaa waxaana laga jarayaa rasiidka qofka freelacerka shaqadaa hayay ee dhamayn wayay <span>3%</span> qiimaha dalabkaa uu ku dhamayn waayay wakhtigiisii . 
-                    Waa Hadii rasiidkiisa lacag xidhan ama furan ugu jirtay hadii kalena waxaa laga hakinayaa akoonkiisa inta uu soo bixinayo lacagta ganaaxa ah  .
-                    </p>
-                    <p className="qodob">
-                    21.	Waxaa mamuuca in freelancerku u diro qofka uu u shaqaynayo waxyaabo lagu wada xidhiidho oo ka baxsan shabakada  sida inuu qof uu adeeg u qabanayo u qoro lanbarkiisa ama mid kamida akoonadiisa baraha bulshadu ku wada xidhiidho arinkaasi waa ka mamnuuc laba dhinacba freelancer -ka iyo macmiilkaba .
-                    Ciqaabtiisuna waxay noqon kartaa in la joojiyo akoonka freelancerka muddo ku siman 30-cisho  mudadaasi marka ay dhamaato si live ah ama si caadiya uu ula kulmo maamulka shabakada si xal looga gaadho qaladkaa uu sameeyay .
-                    </p>
-                    <p className="qodob">
-                    22.	Waa in freelcer-ku aanu soo gallin suuqa wax kasta oo ka hor imanaya xeerarka suuqa  
-                    </p>
-                    <p className="qodob">
-                    23.	qof kasta oo khilaafa shuuradahan iyo qodobada kor ku xusan waxa uu maamulku ku ciqaabi kartaa waxyaabahan hoose <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	 akoon joojin <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	Shaqo joojin <br/>
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	Ka jarid lacag rasiidkiisa 10% hadii rasiidkiisa wax ku jiraan <br/> 
-                    <FontAwesomeIcon icon={faCircleCheck} className="i" />	Nisqaaminta qiimaynta freelancer-ka 20% . <br/>
-                    </p>
-                    
-
                 </div>
             </div>
             {/* <!---------------dhamaadka shaqooyinka -------------------> */}
